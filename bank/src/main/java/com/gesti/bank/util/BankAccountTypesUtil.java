62392a0f211e617715e3d7ad75de14f129f7ab08
Marko1212 <shihonage7@gmail.com>
2020-08-12 15:47:02 +0200

    commit

--- bank/src/main/java/com/gesti/bank/util/BankAccountTypesUtil.java
+++ bank/src/main/java/com/gesti/bank/util/BankAccountTypesUtil.java
@@ -13,6 +13,7 @@ public class BankAccountTypesUtil {
 	public static final String CURRENT_LIMIT_10000_BANK_ACCOUNT_TYPE_NAME = "Current-LIMIT-10000";
 	public static final String CURRENT_LIMIT_20000_BANK_ACCOUNT_TYPE_NAME = "Current-LIMIT-20000";
 	public static final String CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_NAME = "Current-LIMIT-50000";
+	public static final String SAVING_BANK_ACCOUNT_TYPE_NAME = "Saving";
 	
 	private static final Integer CURRENT_BANK_ACCOUNT_TYPE_AMOUNT = 0;
 	private static final Integer CURRENT_LIMIT_1000_BANK_ACCOUNT_TYPE_AMOUNT = 1000;
@@ -21,7 +22,8 @@ public class BankAccountTypesUtil {
 	private static final Integer CURRENT_LIMIT_5000_BANK_ACCOUNT_TYPE_AMOUNT = 5000;
 	private static final Integer CURRENT_LIMIT_10000_BANK_ACCOUNT_TYPE_AMOUNT = 10000;
 	private static final Integer CURRENT_LIMIT_20000_BANK_ACCOUNT_TYPE_AMOUNT = 20000;
-	private static final Integer CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_AMOUNT = 50000; 
+	private static final Integer CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_AMOUNT = 50000;
+	private static final Integer SAVING_BANK_ACCOUNT_TYPE_AMOUNT = 0;
 	
 	private static Map<String, Integer> getBankAccountTypes(){
 		Map<String, Integer> bankAccountTypesMap = new HashedMap();
@@ -32,7 +34,8 @@ public class BankAccountTypesUtil {
 		bankAccountTypesMap.put(CURRENT_LIMIT_5000_BANK_ACCOUNT_TYPE_NAME, CURRENT_LIMIT_5000_BANK_ACCOUNT_TYPE_AMOUNT);
 		bankAccountTypesMap.put(CURRENT_LIMIT_10000_BANK_ACCOUNT_TYPE_NAME, CURRENT_LIMIT_10000_BANK_ACCOUNT_TYPE_AMOUNT);
 		bankAccountTypesMap.put(CURRENT_LIMIT_20000_BANK_ACCOUNT_TYPE_NAME, CURRENT_LIMIT_20000_BANK_ACCOUNT_TYPE_AMOUNT);
-		bankAccountTypesMap.put(CURRENT_LIMIT_5000_BANK_ACCOUNT_TYPE_NAME, CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_AMOUNT);
+		bankAccountTypesMap.put(CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_NAME, CURRENT_LIMIT_50000_BANK_ACCOUNT_TYPE_AMOUNT);
+		bankAccountTypesMap.put(SAVING_BANK_ACCOUNT_TYPE_NAME, SAVING_BANK_ACCOUNT_TYPE_AMOUNT);
 		return bankAccountTypesMap;
 	}
 	
