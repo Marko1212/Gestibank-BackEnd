8b01996824690df387bb772272c6dc35fb441b69
Marko1212 <shihonage7@gmail.com>
2021-06-20 21:55:01 +0200

    hashing password started

--- bank/src/main/java/com/gesti/bank/BankApplication.java
+++ bank/src/main/java/com/gesti/bank/BankApplication.java
@@ -5,10 +5,11 @@ import javax.annotation.Resource;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import com.gesti.bank.service.FilesStorageService;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BankApplication implements CommandLineRunner {
 	@Resource
 	FilesStorageService storageService;
